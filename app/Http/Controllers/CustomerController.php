f4513160b296ca4bbdc64162a44121c881c37f5f
SamantaGomez <samygomezjacome@gmail.com>
2020-07-09 17:08:37 -0500

    #4 Controladores

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    { return Customer::all(); }
+    public function show(Customer $customer)
+    { return $customer; }
+    public function store(Request $request)
+    { return response()->json($customer, 201); }
+    public function update(Request $request, Customer $article)
+    { $customer = Customer::findOrFail($id);
+        $customer->update($request->all());
+        return response()->json($customer, 200); }
+    public function delete(Request $request, $id)
+    { $customer = Customer::findOrFail($id);
+        $customer->delete(); return 204; }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    { return Product::all(); }
+    public function show(Product $product)
+    { return $product; }
+    public function store(Request $request)
+    { return response()->json($product, 201); }
+    public function update(Request $request, Product $article)
+    { $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return response()->json($product, 200); }
+    public function delete(Request $request, $id)
+    { $product = Product::findOrFail($id);
+        $product->delete(); return 204; }
 }
