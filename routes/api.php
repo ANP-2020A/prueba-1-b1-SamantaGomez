367bb4be4b61517f3e3450d0f9d97074ebde0d07
SamantaGomez <samygomezjacome@gmail.com>
2020-07-31 20:18:10 -0500

    union a la dev

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,7 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
+
 
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
@@ -27,7 +24,6 @@ Route::get('products/{product}', 'ProductsController@show');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', 'UserController@getAuthenticatedUser');
-
     Route::post('products', 'ProductsController@store');
     Route::put('products/{product}', 'ProductsController@update');
     Route::delete('products/{product}', 'ProductsController@delete');
